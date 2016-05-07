2f3c5b8e4e5b06560190243dc088e2568f1b1752
Alexander Serrano Professional <Alexander@Alexanders-MacBook-Pro-3.local>
2016-05-07 17:02:33 -0400

    max this is what the authors tried, which performs worse, include figures but feed in str as fhss_past  ofdm_past, in order to prevent overwriting the figures

---
+++ past_approach.m
@@ -0,0 +1,128 @@
+function [ f1, f2, f3, f4,f5, t, f, PSD , PSD_binary,B, centds, horizontal_projection,... 
+         vertical_projection ]   ...
+        = past_approach( S, threshold, fs, str)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+%t = 0:0.01:10;
+%fs = 100; 
+%x = sin(2*pi* 10* t) + 0.5 * sin(2*pi*5*t) ; 
+[s f t p] = spectrogram(S,fs);
+PSD = 10*log10(p);
+f = f*fs/pi ; 
+f1 = figure;
+surf(t,f,PSD,'edgecolor','none'); axis tight; 
+view(0,90); 
+colorbar; 
+xlabel('Time (s) ','Fontsize',12);
+ylabel('Frequency (Hz)','Fontsize',12);
+zlabel('PSD (t,f)','Fontsize',12);
+
+title(['Original PSD', ' ' ,'(',str,')'],'Fontsize',16);  
+
+
+saveas(f1,strcat(str, '_Original_PSD'),'png');
+
+
+%%
+%threshold = 0.1*max(PSD(:)) ; 
+PSD_binary = double(PSD > threshold);
+
+
+%%
+%clc; clear all ; close all; 
+%dilation neighborhood
+dil = strel('square',2 )
+
+
+
+er = strel('square',2)
+
+%erosion neighborhood 
+
+
+%
+% A =   [ zeros(1,12 ) ; zeros(1,5 ), 1 ,zeros(1,5) 1 ;  ... 
+%         0 , 1 ,zeros(1,2) , ones(1,1) , zeros(1,4) , 1 zeros(1,2) ; ...
+%         ones(1,3) , zeros(1,5), ones(1,4)   ; ... 
+%         ones(1,3) , zeros(1,5), ones(1,3), 0 ; ...
+%         ones(1,3) , zeros(1,5), ones(1,3), 0 ; ...
+%         zeros(1,12) ; ...
+%         zeros(1,5) , 1, zeros( 1,4), 1 , 0     ;   ... 
+%         zeros(1,12) ; ...
+%         zeros(1,12) ; ...
+%         zeros(1,12) ; ...
+%         ones(1,3) , zeros(1,5), ones(1,3), 0 ; ...
+%         1, 0 , 1 ,  zeros(1,5), 1, 0 , 1, 0 ; ...
+%         ones(1,3) , zeros(1,5), ones(1,3), 0 ; ...
+%         zeros(1,12) ; ...
+%         zeros(1,12) ; ...
+%         zeros(1,12) ; ...
+%         zeros(1,12) ; ...
+        
+%         zeros(1,8), ones(1,3), 0 ; ...
+%         zeros(1,4),1, zeros(1,3), 1 , 1, 0 , 0 ; ...
+%         zeros(1,8), 0 , 1, 0 , 0 ;...   
+%         zeros(1,12) ; ...
+%         zeros(1,12) ; ...
+%          ] ;
+
+% %
+%  A = padarray(A, [ 4,4]);
+% %I2 = imdilate(I1,dil) ;
+% %
+% I1 = imdilate(A,dil)  ;
+% I2 = imerode(I1,er) ;
+% %
+
+
+I1 = imdilate(PSD_binary,dil)  ;
+B = imerode(I1,er);
+
+
+
+
+
+%figure; imshow(A,'InitialMagnification', 800); 
+f2 = figure; imagesc(f,t,PSD_binary); 
+title(['PSD Threshold Binary Image', ' ', 'T =','',num2str(threshold),' ' ,'(',str,')'],'Fontsize',16);
+xlabel('Frequency, (Hz)','Fontsize',12);
+ylabel('Time (s)','Fontsize',12); 
+colorbar;
+saveas(f2,strcat(str, 'PSD_Threshold_Binary_Image', '_', 'T =','_',num2str(threshold)),'png');
+
+f3 = figure; imagesc(f,t,B);  
+title(['PSD Median Filtered Binary Image', ' ', 'T =','',num2str(threshold), ' ' ,'(',str,')'],'Fontsize',16);
+xlabel('Frequency, (Hz)','Fontsize',12);
+ylabel('Time (s)','Fontsize',12);
+colorbar;
+saveas(f3,strcat(str,'PSD_Median_Filtered_Binary_Image', '_', 'T =','_',num2str(threshold)),'png');
+
+D = bwlabel(B);
+
+measurements = regionprops(D,'Centroid');
+
+centds = measurements.Centroid ;
+
+horizontal_projection = sum(B , 2) ;
+
+max_h = max(horizontal_projection) ; 
+horizontal_coords = find( horizontal_projection >= 0.1*max_h) ; 
+
+vertical_projection = sum(B, 1) ; 
+%%
+f4 =  figure; plot(f,horizontal_projection) ;
+xlabel('Frequency (Hz)','Fontsize',12);
+ylabel('Total Number of Pixels','Fontsize',12);
+grid on ; 
+title(['Projection along Time Axis',' ' ,'(',str,')'],'Fontsize',16);
+saveas(f4,strcat(str,'Projection along Time Axis', '_', 'T =','_',num2str(threshold)),'png');
+
+f5 = figure; plot(t,vertical_projection) ;
+xlabel('Time (s)','Fontsize',12);
+ylabel('Total Number of Pixels','Fontsize',12);
+grid on; 
+title(['Projection along Frequency Axis',' ' ,'(',str,')'],'Fontsize',16);
+saveas(f5,strcat(str,'Projection along Frequency Axis', '_', 'T =','_',num2str(threshold)),'png');
+
+end
+
